2d34ddc518910f85a72ef217b1ee1131038e37d4
Michael Webster <miketwebster@gmail.com>
2016-07-10 20:40:05 -0400

    pam-helper-service.py: terminate 30 seconds after the last activity, instead of on success only.

--- src/pam-helper-service.py
+++ src/pam-helper-service.py
@@ -13,6 +13,8 @@ import constants as c
 
 class PAMHelperService(dbus.service.Object):
     def __init__(self):
+        self.terminate_id = 0
+
         bus_name = dbus.service.BusName(c.PAM_SERVICE, bus=dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, c.PAM_PATH)
 
@@ -20,8 +22,11 @@ class PAMHelperService(dbus.service.Object):
     def authenticate(self, user, password):
         success, msg = authenticator.real_check_password(user, password)
 
-        if success:
-            GObject.timeout_add_seconds(10, ml.quit)
+        if self.terminate_id > 0:
+            GObject.source_remove(self.terminate_id)
+            self.terminate_id = 0
+
+        self.terminate_id = GObject.timeout_add_seconds(30, ml.quit)
 
         return (success, msg)
 
