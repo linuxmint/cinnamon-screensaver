cf2d05ef59acdce1d103824883b1ef40b5e48179
leigh123linux <leigh123linux@googlemail.com>
2016-12-15 18:04:41 +0000

    Fix python shebang

--- src/cinnamon-screensaver-command.py
+++ src/cinnamon-screensaver-command.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
 import gi
 gi.require_version('CScreensaver', '1.0')
--- src/util/eventHandler.py
+++ src/util/eventHandler.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
 from gi.repository import Gdk
 
