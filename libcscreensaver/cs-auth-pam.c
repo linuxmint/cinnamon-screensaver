be9ed17a6725474632a42cd7d7d8f9a73fdd52bd
Michael Webster <miketwebster@gmail.com>
2017-02-05 20:39:19 -0500

    cs-auth-pam.c: Only attempt to remove the io watch if thread_done is FALSE.  It is set to TRUE in the GIOFunc cs_auth_loop_quit, which also returns FALSE, signalling to remove the source.  We can't zero out watch_id there, which was causing a warning when cs_auth_pam_verify_user exited.

--- libcscreensaver/cs-auth-pam.c
+++ libcscreensaver/cs-auth-pam.c
@@ -653,7 +653,7 @@ cs_auth_pam_verify_user (pam_handle_t *handle,
         auth_status = GPOINTER_TO_INT (g_thread_join (auth_thread));
 
  out:
-        if (watch_id != 0) {
+        if (watch_id != 0 && !thread_done) {
                 g_source_remove (watch_id);
                 watch_id = 0;
         }
