7b05a70bc21f7c0bfc2dc3a09518c3ba5b0227cd
Michael Webster <miketwebster@gmail.com>
2022-09-08 10:47:36 -0400

    Fix grabber commenting

--- libcscreensaver/cs-event-grabber.c
+++ libcscreensaver/cs-event-grabber.c
@@ -169,7 +169,7 @@ cs_event_grabber_get_keyboard (CsEventGrabber    *grab,
         g_return_val_if_fail (window != NULL, FALSE);
         g_return_val_if_fail (screen != NULL, FALSE);
 
-        DEBUG ("Grabbing keyboard widget=%X\n", (guint32) GDK_WINDOW_XID (window));
+        DEBUG ("Grabbing keyboard widget=0x%X\n", (guint32) GDK_WINDOW_XID (window));
         status = gdk_keyboard_grab (window, FALSE, GDK_CURRENT_TIME);
 
         if (status == GDK_GRAB_SUCCESS) {
@@ -204,7 +204,7 @@ cs_event_grabber_get_mouse (CsEventGrabber    *grab,
 
         cursor = gdk_cursor_new (GDK_BLANK_CURSOR);
 
-        DEBUG ("Grabbing mouse widget=%X\n", (guint32) GDK_WINDOW_XID (window));
+        DEBUG ("Grabbing mouse widget=0x%X\n", (guint32) GDK_WINDOW_XID (window));
         status = gdk_pointer_grab (window, TRUE, 0, NULL,
                                    (hide_cursor ? cursor : NULL),
                                    GDK_CURRENT_TIME);
@@ -302,11 +302,11 @@ cs_event_grabber_move_mouse (CsEventGrabber    *grab,
         return TRUE;
 #else
         if (grab->priv->mouse_grab_window) {
-                DEBUG ("Moving pointer grab from %X to %X\n",
+                DEBUG ("Moving pointer grab from 0x%X to 0x%X\n",
                           (guint32) GDK_WINDOW_XID (grab->priv->mouse_grab_window),
                           (guint32) GDK_WINDOW_XID (window));
         } else {
-                DEBUG ("Getting pointer grab on %X\n",
+                DEBUG ("Getting pointer grab on 0x%X\n",
                           (guint32) GDK_WINDOW_XID (window));
         }
 #endif
@@ -357,11 +357,11 @@ cs_event_grabber_move_keyboard (CsEventGrabber    *grab,
         }
 
         if (grab->priv->keyboard_grab_window != NULL) {
-                DEBUG ("Moving keyboard grab from %X to %X\n",
+                DEBUG ("Moving keyboard grab from 0x%X to 0x%X\n",
                           (guint32) GDK_WINDOW_XID (grab->priv->keyboard_grab_window),
                           (guint32) GDK_WINDOW_XID (window));
         } else {
-                DEBUG ("Getting keyboard grab on %X\n",
+                DEBUG ("Getting keyboard grab on 0x%X\n",
                           (guint32) GDK_WINDOW_XID (window));
 
         }
